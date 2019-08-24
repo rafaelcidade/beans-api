1f9d16d794cf51a434198a065466fe958ddb1289
Rafael Cidade <rafael@pillarproject.io>
2019-08-24 16:12:21 +0200

    cors

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ mongoose.connect(mongoUri, { useNewUrlParser: true }).then(
         console.log('Database is connected');
   });
 
-// app.get('/get-quiz', cors({ origin: false }));
-// app.post('/check-answer', cors({ origin: false }));
-// app.post('/insert-quiz', cors({ origin: false }));
-app.use(cors({ origin: false }));
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
